fba42e92f7c27629c51f4bda43bcbb05582054b4
Juan José Hernández Martínez <juanjose_hm@yahoo.com>
2025-11-06 17:34:10 -0600

    Add files via upload

---
+++ src/main/java/com/delicias/soft/services/delicias_kafka_consumer/DeliciasKafkaConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.delicias.soft.services.delicias_kafka_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliciasKafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliciasKafkaConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/delicias/soft/services/delicias_kafka_consumer/DeliciasKafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delicias.soft.services.delicias_kafka_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliciasKafkaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
